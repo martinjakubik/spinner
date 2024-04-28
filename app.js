ae8ad2d8ca3b02a12b8d6aab9f0f13f382d29001
martin jakubik <martin.jakubik@sap.com>
2024-04-28 22:16:00 +0200

    passed shape parameter through whole stack

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const getFormattedPointForClockTimer = function (aPoint, index, nNumberOfPoints)
     return Math.floor(aPoint[0] + nCosine * nScale) + 'px ' + Math.floor(aPoint[1] + nSine * nScale) + 'px';
 }
 
+const getFormattedPointForDotSpinner = function (aPoint, index, nNumberOfPoints) {
+    const nScale = 40;
+    const nIncrement = index * 2 * Math.PI / nNumberOfPoints - Math.PI / 2;
+    const nCosine = Math.cos(nIncrement);
+    const nSine = Math.sin(nIncrement);
+    return Math.floor(aPoint[0] + nCosine * nScale) + 'px ' + Math.floor(aPoint[1] + nSine * nScale) + 'px';
+}
+
 const getFormattedPoint = function (eShape = SHAPES.CLOCK_TIMER, aPoint, index, nNumberOfPoints) {
     switch (eShape) {
         case SHAPES.CLOCK_TIMER:
@@ -20,17 +28,17 @@ const getFormattedPoint = function (eShape = SHAPES.CLOCK_TIMER, aPoint, index, 
     }
 }
 
-const getShape = function (nTicks) {
+const getShape = function (eShape = SHAPES.CLOCK_TIMER, nTicks) {
     const nNumberOfPoints = 30;
     const aStartPoint = [80, 80];
     let aFormattedPoints = [];
     const nTicksByPoints = Math.floor(nTotalTicks / nNumberOfPoints);
-    aFormattedPoints.push(getFormattedPoint(SHAPES.CLOCK_TIMER, aStartPoint, 0, nNumberOfPoints));
+    aFormattedPoints.push(getFormattedPoint(eShape, aStartPoint, 0, nNumberOfPoints));
     aFormattedPoints.push(Math.floor(aStartPoint[0]) + 'px ' + Math.floor(aStartPoint[1]) + 'px');
     if (nTicks > 0) {
         for (let nPoint = 1; nPoint < nNumberOfPoints; nPoint++) {
             if (nTicks < nPoint * nTicksByPoints) {
-                aFormattedPoints.push(getFormattedPoint(SHAPES.CLOCK_TIMER, aStartPoint, nPoint, nNumberOfPoints))
+                aFormattedPoints.push(getFormattedPoint(eShape, aStartPoint, nPoint, nNumberOfPoints))
             }
         }
     }
@@ -38,14 +46,14 @@ const getShape = function (nTicks) {
     return 'polygon(' + sShapePoints + ')';
 }
 
-const drawSpinner = function () {
+const drawSpinner = function (eShape = SHAPES.CLOCK_TIMER) {
     const oSpinner = document.getElementById('spinner');
     if (nNumberOfTicks < nTotalTicks) {
-        const sShape = getShape(nNumberOfTicks);
+        const sShape = getShape(eShape, nNumberOfTicks);
         oSpinner.style.clipPath = sShape;
         nNumberOfTicks = nNumberOfTicks + 1;
     } else {
-        const sShape = getShape(0);
+        const sShape = getShape(eShape, 0);
         oSpinner.style.clipPath = sShape;
         clearInterval(nIntervalIdSpinner);
     }
@@ -60,8 +68,8 @@ const main = function () {
     oButtonRestart.onclick = main;
     nNumberOfTicks = 0;
     clearInterval(nIntervalIdSpinner);
-    nIntervalIdSpinner = setInterval(drawSpinner, 700);
-    drawSpinner();
+    nIntervalIdSpinner = setInterval(drawSpinner, 700, SHAPES.CLOCK_TIMER);
+    drawSpinner(SHAPES.CLOCK_TIMER);
 }
 
 main();
