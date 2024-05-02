2200d5427ddefd98992b2c9eb73290f89e5986f4
martin jakubik <martin.jakubik@sap.com>
2024-05-02 16:09:21 +0200

    removed sine wave code from clock timer class

--- clockTimerSpinner.js
+++ clockTimerSpinner.js
@@ -1,7 +1,7 @@
 import { BaseSpinner } from "./baseSpinner.js";
 
 export class ClockTimerSpinner extends BaseSpinner {
-    static getPointForClockTimer = function (aPoint, index, nNumberOfPoints, bIsCssFormat = true) {
+    static getPoint = function (aPoint, index, nNumberOfPoints, bIsCssFormat = true) {
         const nScale = 40;
         const nIncrement = index * 2 * Math.PI / nNumberOfPoints - Math.PI / 2;
         const nCosine = Math.cos(nIncrement);
@@ -11,36 +11,16 @@ export class ClockTimerSpinner extends BaseSpinner {
         return bIsCssFormat ? ClockTimerSpinner.getCssFormattedPointFromXY(sFormattedXValue, sFormattedYValue) : ClockTimerSpinner.getSvgPointFromXY(sFormattedXValue, sFormattedYValue);
     }
 
-    static getPointForSineWave = function (aPoint, index, nNumberOfPoints, bIsCssFormat = true) {
-        const nScale = 40;
-        const nIncrement = index * 2 * Math.PI / nNumberOfPoints - Math.PI / 2;
-        const nSine = Math.sin(nIncrement);
-        const sFormattedXValue = Math.floor(aPoint[0] - 74 + nNumberOfPoints - index) * 4;
-        const sFormattedYValue = Math.floor(aPoint[1] + nSine * nScale);
-        return bIsCssFormat ? ClockTimerSpinner.getCssFormattedPointFromXY(sFormattedXValue, sFormattedYValue) : ClockTimerSpinner.getSvgPointFromXY(sFormattedXValue, sFormattedYValue);
-    }
-
-    static getPoint = function (eShape = ClockTimerSpinner.SHAPES.CLOCK_TIMER, aPoint, index, nNumberOfPoints, bIsCssFormat = true) {
-        switch (eShape) {
-            case ClockTimerSpinner.SHAPES.CLOCK_TIMER:
-                return ClockTimerSpinner.getPointForClockTimer(aPoint, index, nNumberOfPoints, bIsCssFormat)
-            case ClockTimerSpinner.SHAPES.SINE_WAVE:
-                return ClockTimerSpinner.getPointForSineWave(aPoint, index, nNumberOfPoints, bIsCssFormat)
-            default:
-                return ClockTimerSpinner.apply.getPointForClockTimer(aPoint, index, nNumberOfPoints, bIsCssFormat)
-        }
-    }
-
     static getShapePathCss = function (eShape = ClockTimerSpinner.SHAPES.CLOCK_TIMER, nTicks, nNumberOfPoints, aStartPoint, nTicksByPoints) {
         let aFormattedPoints = [];
-        let oFormattedPoint = ClockTimerSpinner.getPoint(eShape, aStartPoint, 0, nNumberOfPoints, true);
+        let oFormattedPoint = ClockTimerSpinner.getPoint(aStartPoint, 0, nNumberOfPoints, true);
         aFormattedPoints.push(oFormattedPoint);
         const oFormattedStartPoint = ClockTimerSpinner.getCssFormattedPointFromXY(aStartPoint[0], aStartPoint[1], true);
         aFormattedPoints.push(oFormattedStartPoint);
         if (nTicks > 0) {
             for (let nPoint = 1; nPoint < nNumberOfPoints; nPoint++) {
                 if (nTicks < nPoint * nTicksByPoints) {
-                    oFormattedPoint = ClockTimerSpinner.getPoint(eShape, aStartPoint, nPoint, nNumberOfPoints, true);
+                    oFormattedPoint = ClockTimerSpinner.getPoint(aStartPoint, nPoint, nNumberOfPoints, true);
                     aFormattedPoints.push(oFormattedPoint);
                 }
             }
@@ -54,14 +34,14 @@ export class ClockTimerSpinner extends BaseSpinner {
     static getShapePathSvg = function (eShape = ClockTimerSpinner.SHAPES.CLOCK_TIMER, nTicks, nNumberOfPoints, aStartPoint, nTicksByPoints) {
         const oSvgSpinner = document.getElementById('svgspinner');
         const oSvgPointList = oSvgSpinner.points;
-        let oFormattedPoint = ClockTimerSpinner.getPoint(eShape, aStartPoint, 0, nNumberOfPoints, false);
+        let oFormattedPoint = ClockTimerSpinner.getPoint(aStartPoint, 0, nNumberOfPoints, false);
         oSvgPointList.appendItem(oFormattedPoint);
         const oFormattedStartPoint = ClockTimerSpinner.getSvgPointFromXY(aStartPoint[0], aStartPoint[1], false);
         oSvgPointList.appendItem(oFormattedStartPoint);
         if (nTicks > 0) {
             for (let nPoint = 1; nPoint < nNumberOfPoints; nPoint++) {
                 if (nTicks < nPoint * nTicksByPoints) {
-                    oFormattedPoint = ClockTimerSpinner.getPoint(eShape, aStartPoint, nPoint, nNumberOfPoints, false);
+                    oFormattedPoint = ClockTimerSpinner.getPoint(aStartPoint, nPoint, nNumberOfPoints, false);
                     oSvgPointList.appendItem(oFormattedPoint);
                 }
             }
