07e8056795da962d2ed46e67c656c5800cb3c4e6
martin jakubik <martin.jakubik@sap.com>
2024-05-02 17:10:47 +0200

    removed shape list from base class

--- baseSpinner.js
+++ baseSpinner.js
@@ -1,9 +1,4 @@
 export class BaseSpinner {
-    static SHAPES = {
-        CLOCK_TIMER: 0,
-        SINE_WAVE: 1
-    }
-
     static getSvgPointFromXY = function (sFormattedXValue, sFormattedYValue) {
         const oSvgContent = document.getElementById('svgcontent');
         const oSvgPoint = oSvgContent.createSVGPoint();
